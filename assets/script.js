8bac661cd3d25fddb101dda11e0a4b1cd9d5ef86
unknown <mindydiane@gmail.com>
2021-05-30 20:19:25 -0500

    fixed other half of var chars to include special and lower case

--- assets/script.js
+++ assets/script.js
@@ -10,8 +10,10 @@ var numberChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 numberChars = numberChars.join("")
 var upperChars = ["A","B","C","D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
 upperChars = upperChars.join("")
-const lowerChars = ["a", "b", "c", "d", "e","f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+var lowerChars = ["a", "b", "c", "d", "e","f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+lowerChars = lowerChars.join("")
+var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+specialChars = specialChars.join("")
 
 // Write password to the #password input
 function writePassword() {
