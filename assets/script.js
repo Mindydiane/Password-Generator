c0dbd79dc21a7d91b42d63685e28813e022f36b2
unknown <mindydiane@gmail.com>
2021-05-30 20:13:52 -0500

    add variables, if, generates w/,

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,17 @@
 // Assignment code here
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var combinedArray = []
+var characterSelection;
+var tempPassword = []
+
+// variables for
+var numberChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+numberChars = numberChars.join("")
+var upperChars = ["A","B","C","D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+upperChars = upperChars.join("")
+const lowerChars = ["a", "b", "c", "d", "e","f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
 
 // Write password to the #password input
 function writePassword() {
@@ -11,20 +22,71 @@ function writePassword() {
 
 }
 
+
 function generatePassword() {
   var passwordLength = prompt("how long would you like your password? between 8-128 characters.")
-  // return Math.floor(Math.random() * 11)
+  
+  if(passwordLength >= 8 || passwordLength <= 128) {
+  // passwordLength = alert("Outside boundaries, has to be within 8-128")
+  
+ 
+    var lowerCharacter = confirm("Do you want lower case?");
+    var upperCharacter = confirm("Do you want UPPER CASE?");
+    var specialCharacter = confirm("Would you like special characters?");
+    var numberCharacter = confirm("Would you like numbers?");
+    var temp;
 
-  while(passwordLength < 8 || passwordLength > 128) {
-    passwordLength = prompt("Outside boundaries, has to be within 8-128")
-  }
+    if (lowerCharacter){
+      alert("you chose lower case.")
+      // combinedArray = [].concat(lowerChars)
+      combinedArray.push(lowerChars);
+      console.log(combinedArray);
+    }
+    if (upperCharacter){
+      alert("you chose upper case.")
+      // combinedArray = [].concat(upperChars)
+      combinedArray.push(upperChars);
+      console.log(combinedArray);
+   
+    }
 
-  var randomPass = ""
-  for(i=0; i<passwordLength; i++){
-    randomPass += "a";
+    if (specialCharacter){
+      alert("you chose special characters.")
+      combinedArray.push(specialChars)
+      // combinedArray.push(specialChars)
+      console.log(combinedArray);
+      // console.log(combinedArray.join(""));
+    }
+
+    if (numberCharacter){
+      alert("you chose numbers!")
+      combinedArray.push(numberChars)
+      // combinedArray.push(numberChars)
+      console.log(combinedArray);
+    }
+    //elerate 
+    for(var i = 0; i < passwordLength; i++){
+      temp = combinedArray.join("");
+      
+      console.log(temp);
+      // console.log(combinedArray + 'final array');
+      // temp=combinedArray.replace(/,/g , " ");
+      // combinedArray=temp.replace("")
+      // console.log(combinedArray);
+      characterSelection = temp[Math.floor(Math.random()*temp.length)]
+      console.log(characterSelection);
+      tempPassword.push(characterSelection);
+      // tempPassword = characterSelection.join("");
+      console.log(tempPassword);
+    }
+    return tempPassword;
+  
   }
-  return randomPass
+  else{
+    window.alert("input is out of range.")
+  }
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
